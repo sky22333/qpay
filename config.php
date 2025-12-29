9679283ec03418a75c10dc0f0ecbd9e64638b5e9
user123 <user123@user.com>
2025-12-29 10:13:33 +0800

    增加环境变量

--- config.php
+++ config.php
@@ -3,17 +3,17 @@ if (!defined('IN_SYSTEM')) die('Access Denied');
 
 return [
     // 支付接口域名
-    'PAYMENT_URL' => 'https://pay.example.com/mapi.php',
+    'PAYMENT_URL' => getenv('PAYMENT_URL') ?: 'https://pay.example.com/mapi.php',
     
     // 商户ID
-    'MERCHANT_ID' => '1000',
+    'MERCHANT_ID' => getenv('MERCHANT_ID') ?: '1000',
     
     // 商户KEY
-    'API_KEY' => 'xxxxxxxxxxxxxxxxxxxxxx',
+    'API_KEY' => getenv('API_KEY') ?: 'xxxxxxxxxxxxxxxxxxxxxx',
     
     // 你的域名
-    'NOTIFY_URL' => 'https://example.com/pay/notify.php',
+    'NOTIFY_URL' => getenv('NOTIFY_URL') ?: 'https://example.com/pay/notify.php',
     
     // 你的域名
-    'RETURN_URL' => 'https://example.com/success.html'
+    'RETURN_URL' => getenv('RETURN_URL') ?: 'https://example.com/success.html'
 ];
